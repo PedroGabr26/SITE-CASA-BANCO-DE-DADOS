1aaa1046bd05fef0eadd9f6d51b05293c5e9acec
Pedro Gabriel <programmerp4.sntsfrts@gmail.com>
2025-01-14 10:28:27 -0300

    update search_advanced

--- home.py
+++ home.py
@@ -1,11 +1,32 @@
 import streamlit as st
 
 # Título da aplicação
-st.title("Meu Projeto com Múltiplas Páginas")
+st.title("🏠 Site Casa dos Dados")
 
-# Introdução
 st.write("Bem-vindo à aplicação principal! Use o menu lateral para navegar entre as páginas.")
+st.write("""
+    Esse é um site experimental, no qual utilizamos a api da casa dos dados, e as funções mais importantes da mesma como: 
+    - A busca por um cnpj, o qual você só pode fazer um por vez e ter acesso a variadas informações, desde o nome da empresa até o contato do lead, se o mesmo estiver no banco de dados da api.
+    - E outra função é a busca avançada que por meio dos filtros você consegue econtrar varios CNPJs de uma só vez.
+""")
+st.write("A Casa dos Dados é uma plataforma onde voce encontra dados públicos de CNPJ (Cadastro Nacional de Pessoas Jurídicas) e consegue encontrar formas de identificar essa empresas pelos mais diversos filtros.")
 
-# Adiciona um menu lateral
-st.sidebar.title("Navegação")
-st.sidebar.write("Escolha uma página acima para começar.")
+st.subheader("📊 Objetivos do site")
+
+st.write("""
+Nosso objetivo é oferecer a você, empreendedor ou profissional, uma ferramenta poderosa para potencializar suas estratégias de negócios. 
+Com a integração da API Casa dos Dados, você poderá:
+
+- **Identificar oportunidades de mercado:** Encontre empresas relevantes para suas necessidades com base em filtros avançados como localização, setor de atuação, capital social, entre outros.
+- **Facilitar a prospecção de clientes:** Acesse informações detalhadas e confiáveis de CNPJs, ajudando na tomada de decisão mais assertiva.
+- **Economizar tempo e recursos:** Obtenha dados prontos para uso em poucos cliques, simplificando processos de pesquisa e análise.
+- **Apoiar o crescimento do seu negócio:** Use as informações obtidas para planejar estratégias, estabelecer parcerias ou explorar novos segmentos de mercado.
+
+Seja para expandir seu portfólio, construir relacionamentos comerciais ou realizar análises de mercado, o site está aqui para transformar dados em valor para o seu negócio!
+""")
+
+st.subheader("🔗 Mais Informações")
+url = "https://portal.casadosdados.com.br/"
+st.write("Plataforma Casa dos Dados : [link](%s)" % url)
+api = "https://portal.casadosdados.com.br/docs/api"
+st.write("Documentação API : [link](%s)" % api)
--- pages/search_advanced.py
+++ pages/search_advanced.py
@@ -3,7 +3,7 @@ import requests
 
 st.title("Busca Avançada CNPJ")
 
-# Função para realizar a requisição à API
+# Função para realizar a requisição à API com paginação
 def fazer_requisicao(filtros):
     url = "https://api.casadosdados.com.br/v5/cnpj/pesquisa"
     headers = {
@@ -13,80 +13,97 @@ def fazer_requisicao(filtros):
     # Corpo da requisição com os filtros
     body = {}
 
-    # Adicionando filtros no corpo da requisição, apenas se houver valores
-    if filtros.get('nome_empresa'):
-        body['nome_empresa'] = filtros['nome_empresa']
+    # Adicionando filtros no corpo da requisição
+    if filtros.get('cnpj'):
+        body['cnpj'] = filtros['cnpj']
     if filtros.get('estado'):
         body['estado'] = filtros['estado']
+    if filtros.get('bairro'):
+        body['bairro'] = filtros['bairro']
+    if filtros.get('ddd'):
+        body['ddd'] = filtros['ddd']
+    if filtros.get('nome_empresa'):
+        body['nome_empresa'] = filtros['nome_empresa']
     if filtros.get('municipio'):
         body['municipio'] = filtros['municipio']
     if filtros.get('situacao_cadastral'):
         body['situacao_cadastral'] = filtros['situacao_cadastral']
     if filtros.get('codigo_atividade_principal'):
         body['codigo_atividade_principal'] = filtros['codigo_atividade_principal']
-    
-    # Se as datas de abertura não forem fornecidas, define o valor padrão "2025-01-12"
     if filtros.get('data_abertura_inicio') and filtros.get('data_abertura_fim'):
         body['data_abertura'] = {
             "inicio": filtros['data_abertura_inicio'],
             "fim": filtros['data_abertura_fim'],
-            "ultimos_dias": 0
+            "ultimos_dias": 0  # Mantendo a chave "ultimos_dias" conforme solicitado
         }
-    else:
-        body['data_abertura'] = {
-            "inicio": "2025-01-12",
-            "fim": "2025-01-12",
-            "ultimos_dias": 0
-        }
-
-    if filtros.get('capital_social_minimo') and filtros.get('capital_social_maximo'):
+    if filtros.get('capital_social_minimo') is not None and filtros.get('capital_social_maximo') is not None:
         body['capital_social'] = {
             "minimo": filtros['capital_social_minimo'],
             "maximo": filtros['capital_social_maximo']
         }
+    elif filtros.get('capital_social_minimo') is None and filtros.get('capital_social_maximo') is None:
+        body['capital_social'] = {
+            "minimo": 0,
+            "maximo": 0
+        }
 
-    # Se não houver filtros, o corpo da requisição será vazio
-    if not body:
-        body = {}
+    # Inicializando a página
+    page = 1
+    total_results = 0
+    cnpjs = []
 
-    # Realizando a requisição
-    response = requests.post(url, headers=headers, json=body)
+    while True:
+        body['page'] = page  # Adiciona o número da página na requisição
+        response = requests.post(url, headers=headers, json=body)
 
-    # Verificando a resposta
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return {"erro": response.status_code}
+        if response.status_code == 200:
+            data = response.json()
+            cnpjs_page = data.get('cnpjs', [])
+            total_results += len(cnpjs_page)
+            cnpjs.extend(cnpjs_page)  # Adiciona os resultados da página à lista total
+
+            # Verifica se há uma próxima página
+            next_page = data.get('next_page')  # Substitua "next_page" pelo campo correto, se necessário
+            if next_page:
+                page += 1  # Se houver próxima página, incrementa o número da página
+            else:
+                break  # Se não houver mais páginas, sai do loop
+        else:
+            return {"erro": response.status_code}
+
+    return {"total": total_results, "cnpjs": cnpjs}
+
 
 # Interface com Streamlit
 def app():
     # Campos de input para os filtros
-    nome_empresa = st.text_input("Nome da Empresa", "")
-    estado = st.text_input("Estado (ex: SP)", "")
-    municipio = st.text_input("Município (ex: Guarulhos)", "")  # Adicionando o campo Município
-    situacao_cadastral = st.selectbox("Situação Cadastral", ["", "ATIVA", "INAPTA"])
-    codigo_atividade_principal = st.text_input("Código Atividade Principal (ex: 7020400)", "")
+    cnpj = st.text_input("CNPJ (separados por vírgula)", "")
+    estado = st.text_input("Estado (separados por vírgula)", "")
+    bairro = st.text_input("Bairro (separados por vírgula)", "")
+    ddd = st.text_input("DDD (separados por vírgula)", "")
+    nome_empresa = st.text_input("Nome da Empresa (separados por vírgula)", "")
+    municipio = st.text_input("Município (separados por vírgula)", "")
+    situacao_cadastral = st.selectbox("Situação Cadastral", ["", "ATIVA", "INAPTA", "BAIXADA", "NULA", "SUSPENSA"])
+    codigo_atividade_principal = st.text_input("Código Atividade Principal (separados por vírgula)", "")
     data_abertura_inicio = st.date_input("Data Abertura - Início", None)
     data_abertura_fim = st.date_input("Data Abertura - Fim", None)
     capital_social_minimo = st.number_input("Capital Social Mínimo", min_value=0, step=1000, value=0)
     capital_social_maximo = st.number_input("Capital Social Máximo", min_value=0, step=1000, value=0)
 
-    # Ajustando as datas de abertura para "2025-01-12" se não forem informadas
-    if not data_abertura_inicio and not data_abertura_fim:
-        data_abertura_inicio = "1920-01-12"
-        data_abertura_fim = "2025-01-12"
-
     # Criando o dicionário de filtros, ignorando valores vazios
     filtros = {
-        "nome_empresa": [nome_empresa] if nome_empresa else None,
-        "estado": [estado] if estado else None,
-        "municipio": [municipio] if municipio else None,  # Adicionando município como lista
+        "cnpj": [x.strip() for x in cnpj.split(',')] if cnpj else None,
+        "estado": [x.strip() for x in estado.split(',')] if estado else None,
+        "bairro": [x.strip() for x in bairro.split(',')] if bairro else None,
+        "ddd": [x.strip() for x in ddd.split(',')] if ddd else None,
+        "nome_empresa": [x.strip() for x in nome_empresa.split(',')] if nome_empresa else None,
+        "municipio": [x.strip() for x in municipio.split(',')] if municipio else None,
         "situacao_cadastral": [situacao_cadastral] if situacao_cadastral else None,
-        "codigo_atividade_principal": [codigo_atividade_principal] if codigo_atividade_principal else None,
+        "codigo_atividade_principal": [x.strip() for x in codigo_atividade_principal.split(',')] if codigo_atividade_principal else None,
         "data_abertura_inicio": data_abertura_inicio if data_abertura_inicio else None,
         "data_abertura_fim": data_abertura_fim if data_abertura_fim else None,
         "capital_social_minimo": capital_social_minimo if capital_social_minimo else None,
-        "capital_social_maximo": capital_social_maximo if capital_social_maximo else None,
+        "capital_social_maximo": capital_social_maximo if capital_social_maximo else None
     }
 
     # Botão para realizar a busca
@@ -98,7 +115,17 @@ def app():
         if "erro" in resultados:
             st.error(f"Erro na requisição: {resultados['erro']}")
         else:
-            st.write(resultados)
+            total = resultados.get('total', 0)
+            cnpjs = resultados.get('cnpjs', [])
+            
+            st.write(f"Total de resultados encontrados: {total}")
+            
+            if cnpjs:
+                st.write("CNPJs encontrados:")
+                for cnpj in cnpjs:
+                    st.write(cnpj)  # Exibe cada CNPJ individualmente
+            else:
+                st.write("Nenhum CNPJ encontrado.")
 
 # Rodar a aplicação
 if __name__ == "__main__":
